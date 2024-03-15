5f9238e61362163e170b7b40d821c9ce0620d0ee
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-03-15 13:56:47 -0400

    delete convention plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,35 @@
 rootProject.name = "compass"
-include(":compass-core")
-includeBuild("convention-plugins")
 
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
         mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+plugins {
+    id("com.gradle.enterprise") version "3.16.2"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
+include(":compass-core")
