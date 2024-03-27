0000616688ced1563d7904247e49c5cfac988b70
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-03-27 16:42:59 -0400

    add comments

--- compass-permissions-mobile/src/commonMain/kotlin/dev/jordond/compass/permissions/mobile/PermissionController.kt
+++ compass-permissions-mobile/src/commonMain/kotlin/dev/jordond/compass/permissions/mobile/PermissionController.kt
@@ -2,10 +2,20 @@ package dev.jordond.compass.permissions.mobile
 
 import dev.jordond.compass.permissions.LocationPermissionController
 
+/**
+ * Creates a new [LocationPermissionController] for Android and iOS.
+ *
+ * @return A new [LocationPermissionController].
+ */
 public fun LocationPermissionController(): LocationPermissionController {
     return createPermissionController()
 }
 
+/**
+ * Creates a new [LocationPermissionController] for Android and iOS.
+ *
+ * @return A new [LocationPermissionController].
+ */
 public fun LocationPermissionController.Companion.mobile(): LocationPermissionController {
     return LocationPermissionController()
 }
--- compass-permissions/src/commonMain/kotlin/dev/jordond/compass/permissions/LocationPermissionController.kt
+++ compass-permissions/src/commonMain/kotlin/dev/jordond/compass/permissions/LocationPermissionController.kt
@@ -1,7 +1,8 @@
 package dev.jordond.compass.permissions
 
 import dev.jordond.compass.Priority
-import kotlinx.coroutines.flow.Flow
+import dev.jordond.compass.permissions.exception.PermissionDeniedException
+import dev.jordond.compass.permissions.exception.PermissionDeniedForeverException
 
 /**
  * Defines a controller for managing location permissions.
@@ -21,6 +22,17 @@ public interface LocationPermissionController {
      * @return The state of the permission after the request.
      */
     public suspend fun requirePermissionFor(priority: Priority): PermissionState
+
+    /**
+     * Requests the necessary permissions to access the device's location, and throws an exception
+     * if the permission is denied.
+     *
+     * @throws PermissionDeniedException If the permission is denied.
+     * @throws PermissionDeniedForeverException If the permission is missing.
+     */
+    public suspend fun requirePermissionForOrThrow(priority: Priority) {
+        requirePermissionFor(priority).also { it.throwOnError() }
+    }
 
     public companion object
 }
