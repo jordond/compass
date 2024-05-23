aa312a322f7db4f5b4c446648fe3f2529ed72e45
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-05-23 18:37:59 -0400

    fix kotlin 2.0 and get rid of warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 plugins {
     alias(libs.plugins.multiplatform) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
