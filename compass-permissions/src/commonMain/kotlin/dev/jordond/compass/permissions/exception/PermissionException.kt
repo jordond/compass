53cd303244be60bb50bd4a1dc2e8d50068ee45e0
Cristan Meijer <cristan.meijer@gmail.com>
2024-12-19 21:07:00 +0100

    Feature: Clean up exceptions (#106)

--- compass-permissions/src/commonMain/kotlin/dev/jordond/compass/permissions/exception/PermissionException.kt
+++ compass-permissions/src/commonMain/kotlin/dev/jordond/compass/permissions/exception/PermissionException.kt
@@ -6,24 +6,6 @@ package dev.jordond.compass.permissions.exception
 public sealed class PermissionException(message: String) : Throwable(message)
 
 /**
- * Permissions is missing and we will not attempt to request the permission.
- */
-public class PermissionMissingException(permission: String) : PermissionException(
-    "Permission $permission is required to use the Geolocation API"
-)
-
-/**
- * An error occurred while requesting a permission.
- *
- * @param message The error message.
- * @param permission The permission that was requested.
- */
-public class PermissionRequestException(
-    message: String,
-    permission: String = "Location",
-) : PermissionException("Failed to request permission: $permission, because $message")
-
-/**
  * The user denied the permission request.
  *
  * @param permission The permission that was denied.
