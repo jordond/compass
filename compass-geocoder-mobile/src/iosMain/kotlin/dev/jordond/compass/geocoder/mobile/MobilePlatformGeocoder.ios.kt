05b224ce9578c1200a4567f27c7354720a09dde5
Lukáš Anda <lukas.anda@gmail.com>
2025-06-12 10:57:39 +0200

    Add reverse geocoding localization

--- compass-geocoder-mobile/src/iosMain/kotlin/dev/jordond/compass/geocoder/mobile/MobilePlatformGeocoder.ios.kt
+++ compass-geocoder-mobile/src/iosMain/kotlin/dev/jordond/compass/geocoder/mobile/MobilePlatformGeocoder.ios.kt
@@ -6,29 +6,30 @@ import dev.jordond.compass.geocoder.mobile.internal.geocodeOperation
 import dev.jordond.compass.geocoder.mobile.internal.toCoordinates
 import dev.jordond.compass.geocoder.mobile.internal.toPlaces
 import platform.CoreLocation.CLLocation
+import platform.Foundation.NSLocale
 
-internal class IosPlatformGeocoder : MobilePlatformGeocoder {
+internal class IosPlatformGeocoder(private val locale: NSLocale?) : MobilePlatformGeocoder {
 
     override fun isAvailable(): Boolean = true
 
     override suspend fun reverse(latitude: Double, longitude: Double): List<Place> {
         val platformLocation = CLLocation(latitude, longitude)
         return geocodeOperation { listener ->
-            reverseGeocodeLocation(platformLocation, listener)
+            reverseGeocodeLocation(platformLocation, locale, listener)
         }.toPlaces()
     }
 
     override suspend fun placeFromAddress(address: String): List<Place> {
-        return geocodeOperation { listener -> geocodeAddressString(address, listener) }.toPlaces()
+        return geocodeOperation { listener -> geocodeAddressString(address, null, locale, listener) }.toPlaces()
     }
 
     override suspend fun forward(address: String): List<Coordinates> {
         return geocodeOperation { listener ->
-            geocodeAddressString(address, listener)
+            geocodeAddressString(address, null, locale, listener)
         }.toCoordinates()
     }
 }
 
-internal actual fun createPlatformGeocoder(): MobilePlatformGeocoder {
-    return IosPlatformGeocoder()
+internal actual fun createPlatformGeocoder(locale: String?): MobilePlatformGeocoder {
+    return IosPlatformGeocoder(locale?.let(::NSLocale))
 }
